49f765fa8fdc170d13bcfc6358a39c6fc9bb2dca
hyohakupost-gif <hyohaku.post@gmail.com>
2026-05-22 14:59:50 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyLu7NbJI39q2DFQDR9qIbmcVamFIne5lvrJ6gxPRtkCfp2TmO69xUANcAOONZ0hW5Bzg/exec";
+// config.js の末尾などに追記
+const LAST_UPDATE_API_URL = "https://script.google.com/macros/s/AKfycbxKP3hYT3CVaqZRCS8b8qvOK_41JCf5ADt9xr-I7TICG0t4YdlmFNtTXxXfKSI4CqqAtQ/exec";
