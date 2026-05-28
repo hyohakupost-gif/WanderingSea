4f6119a1f3a1971bb415335cae8844f941339ef3
hyohakupost-gif <hyohaku.post@gmail.com>
2026-05-28 10:18:05 +0900

    Update search.js

--- search.js
+++ search.js
@@ -38,6 +38,7 @@ async function search() {
     try {
         const response = await fetch(`${SCRIPT_URL}?name=${encodeURIComponent(name)}&title=${encodeURIComponent(title)}`, { signal });
         const result = await response.json();
+        console.log("RESULT=", result);
         if (signal.aborted) return;
         resultsDiv.innerHTML = "";
 
